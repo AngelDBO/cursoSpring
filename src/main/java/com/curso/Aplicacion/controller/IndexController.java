1710baa8323960d1fc3220a6167bd491d0409627
Angel <angelbedo104@gmail.com>
2023-10-07 16:13:41 -0500

    Entendiendo la directiva de th:eah

--- src/main/java/com/curso/Aplicacion/controller/IndexController.java
+++ src/main/java/com/curso/Aplicacion/controller/IndexController.java
@@ -6,23 +6,42 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/app")
 public class IndexController {
     @GetMapping(value = {"/index", "/", "", "/home"})
-    public String index(Model model){
+    public String index(Model model) {
         model.addAttribute("titulo", "Hola desde spring");
-        return  "index";
+        return "index";
     }
 
     @RequestMapping("/perfil")
-    public String perfil(Model model){
+    public String perfil(Model model) {
         Usuario usuario = new Usuario();
         usuario.setNombre("Pablo");
         usuario.setApellido("Laslo");
+        usuario.setEmail("correo@correo.com");
 
         model.addAttribute("usuario", usuario);
         model.addAttribute("titulo", "Datos del usuario ".concat(usuario.getNombre()));
-        return  "perfil";
+        return "perfil";
+    }
+
+    @RequestMapping("/listar")
+    public String listar(Model model) {
+        List<Usuario> usuarios = new ArrayList<>();
+        usuarios.add(new Usuario("Laslo", "Qwerty", "laslo@correo.com"));
+        usuarios.add(new Usuario("Moto", "Carro", "moto@correo.com"));
+        usuarios.add(new Usuario("Carro", "Honda", "carro@correo.com"));
+        usuarios.add(new Usuario("Lancha", "Yamaha", "lancha@correo.com"));
+        usuarios.add(new Usuario("Yate", "Suzuki", "yate@correo.com"));
+
+        model.addAttribute("titulo", "Listado de usuarios");
+        model.addAttribute("usuarios", usuarios);
+
+        return "listar";
     }
 }
